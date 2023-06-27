60517beea50a5df301034d5ec06a8dd86b949311
Mathieu Pannetrat <mathieupannetrat5@gmail.com>
2023-06-27 11:12:04 +0200

    fix sql and models for real now

--- www/Core/SQL.php
+++ www/Core/SQL.php
@@ -52,47 +52,36 @@ class SQL{
     // si le resultat de cette methode revoi un bool (false) ca veut dire que dans la table il n'y a aucune donné 
     public function existOrNot(array $where):array|bool
     {
-        if(!is_null(static::getTable())){
-            $sqlWhere = [];
-            foreach ($where as $column=>$value) {
-                $sqlWhere[] = "WHEN EXISTS (SELECT 1 FROM ".static::getTable()." WHERE ".$column."=:".$column.") THEN 'the value for column the ".$column." already exists'";
-            }
-            $queryPrepared = self::$connection->prepare("SELECT CASE ".implode("  ", $sqlWhere)." ELSE 'none_exists' END AS column_exists FROM ".static::getTable()." LIMIT 1;");
-            $queryPrepared->setFetchMode( \PDO::FETCH_ASSOC);
-            $queryPrepared->execute($where);
-
-            return $queryPrepared->fetch(); 
+        $sqlWhere = [];
+        foreach ($where as $column=>$value) {
+            $sqlWhere[] = "WHEN EXISTS (SELECT 1 FROM ".static::getTable()." WHERE ".$column."=:".$column.") THEN 'the value for column the ".$column." already exists'";
         }
+        $queryPrepared = self::$connection->prepare("SELECT CASE ".implode("  ", $sqlWhere)." ELSE 'none_exists' END AS column_exists FROM ".static::getTable()." LIMIT 1;");
+        $queryPrepared->setFetchMode( \PDO::FETCH_ASSOC);
+        $queryPrepared->execute($where);
+
+        return $queryPrepared->fetch(); 
+        
     }
 
     public function getOneWhere(array $where)
     {
-        if(!is_null(static::getTable())){
-            $sqlWhere = [];
-            foreach ($where as $column=>$value) {
-                $sqlWhere[] = $column."=:".$column;
-            }
-            $queryPrepared = self::$connection->prepare("SELECT * FROM ".static::getTable()." WHERE ".implode(" AND ", $sqlWhere));
-            $queryPrepared->setFetchMode( \PDO::FETCH_CLASS, get_called_class());
-            $queryPrepared->execute($where);
-            return $queryPrepared->fetch();
+        $sqlWhere = [];
+        foreach ($where as $column=>$value) {
+            $sqlWhere[] = $column."=:".$column;
         }
-        else{
-            die("le nom de la table n'a pas été renseigné et donc l'action sql select ne peut pas se faire");
-        }
+        $queryPrepared = self::$connection->prepare("SELECT * FROM ".static::getTable()." WHERE ".implode(" AND ", $sqlWhere));
+        $queryPrepared->setFetchMode( \PDO::FETCH_CLASS, get_called_class());
+        $queryPrepared->execute($where);
+        return $queryPrepared->fetch();
     }
 
     public function selectAll(): array
     {
-        if(!is_null(static::getTable())){
-            $queryPrepared = self::$connection->prepare("SELECT * FROM ".static::getTable());
-            $queryPrepared->setFetchMode( \PDO::FETCH_CLASS, get_called_class());
-            $queryPrepared->execute();
-            return $queryPrepared->fetchAll();
-        }
-        else{
-            die("le nom de la table n'a pas été renseigné et donc l'action sql select ne peut pas se faire");
-        }
+        $queryPrepared = self::$connection->prepare("SELECT * FROM ".static::getTable());
+        $queryPrepared->setFetchMode( \PDO::FETCH_CLASS, get_called_class());
+        $queryPrepared->execute();
+        return $queryPrepared->fetchAll();
     }
 
     //Exemple de $fkInfos:
@@ -106,88 +95,70 @@ class SQL{
     // ]
     public function selectWithFk(array $fkInfos): array
     {        
-        if(!is_null(static::getTable())){
-            $sqlJoin = [];
-            foreach($fkInfos as $fkInfo){
-                $sqlJoin[] = "JOIN ".$fkInfo["table"]." ON ".static::getTable().".".$fkInfo["foreignKeys"]["originColumn"]."=".$fkInfo["table"].".".$fkInfo["foreignKeys"]["targetColumn"];
-            }
-            $queryPrepared = self::$connection->prepare("SELECT * FROM ".static::getTable()." ".implode(" ", $sqlJoin));
-            $queryPrepared->setFetchMode( \PDO::FETCH_ASSOC);
-            $queryPrepared->execute();
+        $sqlJoin = [];
+        foreach($fkInfos as $fkInfo){
+            $sqlJoin[] = "JOIN ".$fkInfo["table"]." ON ".static::getTable().".".$fkInfo["foreignKeys"]["originColumn"]."=".$fkInfo["table"].".".$fkInfo["foreignKeys"]["targetColumn"];
+        }
+        $queryPrepared = self::$connection->prepare("SELECT * FROM ".static::getTable()." ".implode(" ", $sqlJoin));
+        $queryPrepared->setFetchMode( \PDO::FETCH_ASSOC);
+        $queryPrepared->execute();
 
-            return $queryPrepared->fetchAll();
-        }
-        else{
-            die("le nom de la table n'a pas été renseigné et donc l'action sql select ne peut pas se faire");
-        }
+        return $queryPrepared->fetchAll();
     }
 
     public function insertIntoJoinTable():bool
     {
-        if(!is_null(static::getTable())){
-            $columns = get_object_vars($this);
-            $columnsToExclude = get_class_vars(get_class());
-            $columns = array_diff_key($columns, $columnsToExclude);
 
-            $queryPrepared = self::$connection->prepare("INSERT INTO ".static::getTable().
-                    " (".implode("," , array_keys($columns) ).") 
-                VALUES
-                (:".implode(" , :" , array_keys($columns) ).") ");
-            return $queryPrepared->execute($columns);
-        }
-        else {
-            die("le nom de la table n'a pas été renseigné et donc l'action sql update/insert ne peut pas se faire");
-            return false;
-        }
+        $columns = get_object_vars($this);
+        $columnsToExclude = get_class_vars(get_class());
+        $columns = array_diff_key($columns, $columnsToExclude);
+
+        $queryPrepared = self::$connection->prepare("INSERT INTO ".static::getTable().
+                " (".implode("," , array_keys($columns) ).") 
+            VALUES
+            (:".implode(" , :" , array_keys($columns) ).") ");
+        return $queryPrepared->execute($columns);
+        
     }
 
     public function save(): ResponseSave
     {
-        if(!is_null(static::getTable())){
-            $columns = get_object_vars($this);
-            $columnsToExclude = get_class_vars(get_class());
-            $columns = array_diff_key($columns, $columnsToExclude);
-            
-            $methode = "";
-            
-            if(is_numeric($this->getId()) && $this->getId()>0) {
-                $methode = "update";
-                $sqlUpdate = [];
-                foreach ($columns as $column=>$value) {
-                    $sqlUpdate[] = $column."=:".$column;
-                }
-                $queryPrepared = self::$connection->prepare("UPDATE ".static::getTable().
-                    " SET ".implode(",", $sqlUpdate). " WHERE id=".$this->getId());
-            }else{
-                $methode = "insert";
-                $queryPrepared = self::$connection->prepare("INSERT INTO ".static::getTable().
-                    " (".implode("," , array_keys($columns) ).") 
-                VALUES
-                (:".implode(" , :" , array_keys($columns) ).") ");
+        $columns = get_object_vars($this);
+        $columnsToExclude = get_class_vars(get_class());
+        $columns = array_diff_key($columns, $columnsToExclude);
+        
+        $methode = "";
+        
+        if(is_numeric($this->getId()) && $this->getId()>0) {
+            $methode = "update";
+            $sqlUpdate = [];
+            foreach ($columns as $column=>$value) {
+                $sqlUpdate[] = $column."=:".$column;
             }
-            foreach ($columns as $key => $value) {
-                if (is_bool($value)) {
-                    $columns[$key] = $value ? 'true' : 'false'; // Convertir la valeur booléenne en chaîne de caractères
-                }
+            $queryPrepared = self::$connection->prepare("UPDATE ".static::getTable().
+                " SET ".implode(",", $sqlUpdate). " WHERE id=".$this->getId());
+        }else{
+            $methode = "insert";
+            $queryPrepared = self::$connection->prepare("INSERT INTO ".static::getTable().
+                " (".implode("," , array_keys($columns) ).") 
+            VALUES
+            (:".implode(" , :" , array_keys($columns) ).") ");
+        }
+        foreach ($columns as $key => $value) {
+            if (is_bool($value)) {
+                $columns[$key] = $value ? 'true' : 'false'; // Convertir la valeur booléenne en chaîne de caractères
             }
+        }
 
-            // echo "<pre>";
-            // var_dump($columns);
-            // var_dump($queryPrepared->queryString);
-            // echo "</pre>";
-            $response = new ResponseSave();
-            $response->success = $queryPrepared->execute($columns);
-            if($methode == "insert"){
-                $response->idNewElement = self::$connection->lastInsertId();
-            }else{
-                $response->idNewElement = 0;
-            }
-            return $response;
+        $response = new ResponseSave();
+        $response->success = $queryPrepared->execute($columns);
+        if($methode == "insert"){
+            $response->idNewElement = self::$connection->lastInsertId();
+        }else{
+            $response->idNewElement = 0;
         }
-        else {
-            die("le nom de la table n'a pas été renseigné et donc l'action sql update/insert ne peut pas se faire");
-            return false;
-        }
+        return $response;
+        
         
 
     }
