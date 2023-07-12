1e85c3b97eded24eea7c84fa7030552924d91d55
Gauthier LO <lo.gauthier.glo@gmail.com>
2023-07-12 13:04:20 +0200

    fix : petits changement dans JeuxController.php

--- www/Controllers/JeuxController.php
+++ www/Controllers/JeuxController.php
@@ -5,18 +5,23 @@ namespace App\Controllers;
 use App\Core\View;
 
 use App\Models\Article AS ArticleModel;
-use App\Models\Category_jeux;
-use App\Models\Jeux;
+use App\Models\Game_Category;
+use App\Models\Game;
+use App\Repository\GameCategoryRepository;
+use App\Repository\GameRepository;
 
 class JeuxController
 {
+    private GameRepository $gameRepository;
+    private GameCategoryRepository $gameCategoryRepository;
+    public function __construct() {
+        $this->gameRepository = new GameRepository();
+        $this->gameCategoryRepository = new GameCategoryRepository();
+    }
     public function allgames(){
         $view = new View("Jeux/allGames", "front");
-        $jeuxModel = new Jeux();
-        $categorieJeuxModel = new Category_jeux();
-
-        $jeux = $jeuxModel->selectAll();
-        $categories = $categorieJeuxModel->selectAll();
+        $jeux = $this->gameRepository->selectAll(new Game);
+        $categories = $this->gameCategoryRepository->selectAll(new Game_Category());
 
         $result = [];
         foreach ($jeux as $index => $value){
