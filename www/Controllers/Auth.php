ec991892ef44175075a07a411e235774cb3ae960
nikolasbaele <nikolasbaele@gmail.com>
2023-07-14 15:41:45 +0200

    ajout et amélioration du rest password dans la page profil

--- www/Controllers/Auth.php
+++ www/Controllers/Auth.php
@@ -25,34 +25,37 @@ class Auth
 {
     private UserRepository $userRepository;
     private RoleRepository $roleRepository;
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->userRepository = new UserRepository();
         $this->roleRepository = new RoleRepository();
     }
+
     public function login(): void
     {
         $formConnection = new Connection();
         $view = new View("Auth/connection", "front");
         $view->assign("form", $formConnection->getConfig());
         $formConfig = $formConnection->getConfig();
-        if($formConnection->isSubmited() && $formConnection->isValid()){
+        if ($formConnection->isSubmited() && $formConnection->isValid()) {
             $user = new User();
             $whereSql = ["pseudo" => $_POST['pseudo']];
             $user = $this->userRepository->getOneWhere($whereSql, $user);
 
-                        if(!is_bool($user)){
-                            //si le resultat de getOneWhere est un bool ca veut dire qu'il na pas trouver l'utilisateur
-                            if (password_verify($_POST['password'], $user->getPassword())) {
-                                // Le mot de passe est correct
-                                if(!$user->getEmailConfirmation()) { //tester si l'email est confirmé
-                                    $formConnection->errors[] = "l'email n'est pas confirmer";
-                                } else {
+            if (!is_bool($user)) {
+                //si le resultat de getOneWhere est un bool ca veut dire qu'il na pas trouver l'utilisateur
+                if (password_verify($_POST['password'], $user->getPassword())) {
+                    // Le mot de passe est correct
+                    if (!$user->getEmailConfirmation()) { //tester si l'email est confirmé
+                        $formConnection->errors[] = "l'email n'est pas confirmer";
+                    } else {
                         //get the name of the role of the user
                         $role = new Role();
                         $whereSql = ["id" => $user->getRoleId()];
                         $role = $this->roleRepository->getOneWhere($whereSql, $role);
                         //l'utilisateur est bien connecter du coup on le redirige vers la home page
-                        
+
                         //creer le token jwt et le set en variable session
                         $payload = array(
                             'id' => $user->getId(), // Identifiant de l'utilisateur
@@ -73,14 +76,13 @@ class Auth
 
                         $_SESSION['token'] = $token;
                         header("Location: default");
-                    }    
-                    
+                    }
+
                 } else {
                     // Le mot de passe est incorrect
                     $formConnection->errors[] = $formConfig["inputs"]["password"]["error"];
                 }
-            }
-            else{
+            } else {
                 //pseudo nexiste pas
                 $formConnection->errors[] = $formConfig["inputs"]["pseudo"]["error"];
             }
@@ -93,14 +95,14 @@ class Auth
         $form = new Register();
         $view = new View("Auth/register", "front");
         $view->assign("form", $form->getConfig());
-        
+
         //Form validé ? et correct ?
-        if($form->isSubmited() && $form->isValid()){
+        if ($form->isSubmited() && $form->isValid()) {
             $user = new User();
 
             $role = $this->userRepository->fetchUserRole()["id"];
 
-            if($form->isPhoneNumberValid($_POST['phone_number']) && $form->isPasswordValid($_POST['password'], $_POST['passwordConfirm']) && $form->isFieldsInfoValid($user, ["email"=>$_POST['email'], "pseudo"=>$_POST['pseudo'], "phone_number"=>$_POST['phone_number']])){
+            if ($form->isPhoneNumberValid($_POST['phone_number']) && $form->isPasswordValid($_POST['password'], $_POST['passwordConfirm']) && $form->isFieldsInfoValid($user, ["email" => $_POST['email'], "pseudo" => $_POST['pseudo'], "phone_number" => $_POST['phone_number']])) {
                 $user->setPseudo($_POST['pseudo']);
                 $user->setFirstname($_POST['first_name']);
                 $user->setLastname($_POST['last_name']);
@@ -114,16 +116,16 @@ class Auth
                 //create confirm token for email confirmation 
                 $lengthKey = 20;
                 $key = "";
-                for($i = 0 ; $i < $lengthKey ; $i++){
-                    $key .= mt_rand(0,20);
+                for ($i = 0; $i < $lengthKey; $i++) {
+                    $key .= mt_rand(0, 20);
                 }
                 $user->setConfirmAndResetToken($key);
 
                 $responseQuery = $this->userRepository->save($user);
-                if($responseQuery->success){ //return true | false
+                if ($responseQuery->success) { //return true | false
                     //envoi du mail pour la confirmation du compte:
                     $to = $user->getEmail();
-                    $contentMail = "<b>Hello ".$user->getPseudo().", <a href='http://localhost/email-confirmation?pseudo=".urlencode($user->getPseudo())."&key=".$user->getConfirmAndResetToken()."'> Confirmez votre compte </a></b>";
+                    $contentMail = "<b>Hello " . $user->getPseudo() . ", <a href='http://localhost/email-confirmation?pseudo=" . urlencode($user->getPseudo()) . "&key=" . $user->getConfirmAndResetToken() . "'> Confirmez votre compte </a></b>";
                     $subject = "Confirmation de compte pour notre site Carte chance.";
                     $resultSendMail = SendMailFunction($to, $contentMail, $subject);
 
@@ -142,65 +144,84 @@ class Auth
         header("Location: /");
     }
 
-    public function emailConfirmation(): void {
+    public function emailConfirmation(): void
+    {
         $view = new View("Auth/emailConfirmation", "front");
         $messageInfo = [];
 
-        if(isset($_GET['pseudo'], $_GET['key']) AND !empty($_GET['pseudo']) AND !empty($_GET['key'])) {
+        if (isset($_GET['pseudo'], $_GET['key']) and !empty($_GET['pseudo']) and !empty($_GET['key'])) {
             $pseudo = htmlspecialchars(urldecode($_GET['pseudo']));
             $key = htmlspecialchars($_GET['key']);
 
             $whereSql = ["pseudo" => $pseudo, "confirm_and_reset_token" => $key];
             $result = $this->userRepository->getOneWhere($whereSql, new User);
-            if(is_bool($result)){
+            if (is_bool($result)) {
                 $messageInfo[] = "utilisateur introuvable belec au hack !";
-            }
-            else{
-                if($result->getEmailConfirmation() == true){
+            } else {
+                if ($result->getEmailConfirmation() == true) {
                     $messageInfo[] = "votre compte a deja été confirmé";
-                }
-                else{
+                } else {
                     $result->setEmailConfirmation(true);
                     $responseQuery = $result->save();
-                    if($responseQuery->success){
+                    if ($responseQuery->success) {
                         $messageInfo[] = "votre compte a bien été confirmez, vous pouvez des maintenant vous connecter";
                     }
                 }
-            }        
-        }
-        else{
+            }
+        } else {
             $messageInfo[] = "les parametres de l'url sont incorrect";
         }
         $view->assign("messageInfo", $messageInfo);
     }
 
-    public function resetPassword():void
+    public function resetPassword(): void
     {
-        $view = new View("Auth/resetPassword", "front");
-        $messageInfo = [];
+        if (isset($_GET['pseudo'], $_GET['key'], $_GET['pwd']) and !empty($_GET['pseudo']) and !empty($_GET['key']) and !empty($_GET['pwd'])) {
+            $messageInfo = "Votre mot de passe n'à pas pu etre modifié";
 
-        if(isset($_GET['pseudo'], $_GET['key'], $_GET['pwd']) AND !empty($_GET['pseudo']) AND !empty($_GET['key']) AND !empty($_GET['pwd'])) {
             $pseudo = htmlspecialchars(urldecode($_GET['pseudo']));
             $key = htmlspecialchars($_GET['key']);
             $newPassword = htmlspecialchars($_GET['pwd']);
 
             $whereSql = ["pseudo" => $pseudo, "confirm_and_reset_token" => $key];
             $result = $this->userRepository->getOneWhere($whereSql, new User);
-            if(is_bool($result)){
-                $messageInfo[] = "utilisateur introuvable belec au hack !";
-            }
-            else{
+            if (!is_bool($result)) {
                 $result->setPassword($newPassword);
-                $responseQuery = $result->save();
-                if($responseQuery->success){
-                    $messageInfo[] = "votre mot de passe a bien été modifié";
+                $responseQuery = $this->userRepository->save($result);
+                if ($responseQuery->success) {
+                    $messageInfo = "Votre mot de passe à bien été modifié";
                 }
             }
+            header("Location: /profil?message=$messageInfo");
+
+        } else {
+            $editUserModalForm = new EditProfileFront();
+            $roles = $this->userRepository->fetchRoles();
+            $rolesOption = [];
+            foreach ($roles as $role) {
+                $rolesOption[$role["id"]] = $role["role_name"];
+            }
+
+            $view = new View("Auth/resetPassword", "front");
+            $informationsUser = getAllInformationsFromToken($_SESSION["token"]);
+            $view->assign("editUserForm", $editUserModalForm->getConfig($rolesOption));
+            $view->assign("informationsUser", $informationsUser);
+
+            //create form for reset password
+            $formResetPassword = new ResetPassword();
+            $view->assign("form", $formResetPassword->getConfig());
+            if ($formResetPassword->isSubmited() && $formResetPassword->isValid()) {
+                if ($formResetPassword->isPasswordValid($_POST['password'], $_POST['passwordConfirm'])) {
+                    //send mail for reset password
+                    $to = $informationsUser['email'];
+                    $contentMail = "<b>Hello " . $informationsUser['pseudo'] . ", <a href='http://localhost/reset-password?pseudo=" . urlencode($informationsUser['pseudo']) . "&key=" . $informationsUser['confirmAndResetToken'] . "&pwd=" . $_POST['password'] . "'> Réinitialiser votre mot de passe </a></b>";
+                    $subject = "Réinitialiser votre mot de passe de votre compte Carte chance.";
+                    $resultSendMail = SendMailFunction($to, $contentMail, $subject);
+                    $view->assign("messageInfoSendMail", "Un mail vous a été envoyé pour comfirmer votre changement de mot de passe");
+                }
+            }
+            $view->assign("formErrors", $formResetPassword->errors);
         }
-        else{
-            $messageInfo[] = "les parametres de l'url sont incorrect";
-        }
-        $view->assign("messageInfo", $messageInfo);
     }
 
     public function profile(): void
@@ -232,6 +253,7 @@ class Auth
         }
         $view->assign("formErrors", $formResetPassword->errors);
     }
+
     public function editProfile(): void
     {
         $id = getSpecificDataFromToken($_SESSION['token'], "id");
